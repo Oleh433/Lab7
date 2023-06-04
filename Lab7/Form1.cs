fef302b0f7e122056d200b72a7ad4a4fe671b335
Oleh Pobyvanets <pobyvanets2005@gmail.com>
2023-06-04 18:03:31 +0300

    f

--- Lab7/Form1.cs
+++ Lab7/Form1.cs
@@ -18,19 +18,50 @@ namespace Lab7
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            g = CreateGraphics();
 
-            Point[] points = {new Point(50, 100), new Point(75, 57), new Point(100, 100)};
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
+            g.Clear(Color.White);
+
+            Point[] points = { new Point(50, 100), new Point(75, 57), new Point(100, 100) };
             g.DrawPolygon(Pens.Green, points);
 
-            Point[] points2 = 
+            Point[] points2 =
                 {
-                    new Point(150, 187), new Point(200, 100), new Point(300, 100), 
+                    new Point(150, 187), new Point(200, 100), new Point(300, 100),
                     new Point(350, 187), new Point(300, 273), new Point(200, 273)
                 };
             g.DrawPolygon(Pens.Blue, points2);
 
             g.DrawArc(Pens.Red, 100, 200, 200, 100, 30, 180);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //reset
+            g = CreateGraphics();
+            g.Clear(Color.White);
+
+            g.DrawEllipse(Pens.Yellow, 150, 150, 100, 120);
+
+            //eyes
+            g.DrawEllipse(Pens.Black, 170, 200, 20, 10);
+            g.DrawEllipse(Pens.Black, 210, 200, 20, 10);
+
+            g.DrawEllipse(Pens.Black, 176, 202, 5, 5);
+            g.DrawEllipse(Pens.Black, 216, 202, 5, 5);
+
+            //mouth
+            Point[] points = { new Point(180, 240), new Point(220, 240), new Point(200, 250) };
+            g.DrawClosedCurve(Pens.Red, points);
+
+            //nose
+            g.DrawLine(Pens.Black, 196, 210, 193, 225 );
+            g.DrawLine(Pens.Black, 193, 225, 197, 226);
+
         }
     }
 }
